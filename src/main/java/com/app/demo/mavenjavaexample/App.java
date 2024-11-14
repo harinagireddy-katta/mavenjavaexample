df964c163ac9c7cd4721664363c94cebdc3db0b8
hari <harinagireddykatta@gmail.com>
2024-11-14 15:19:15 +0530

    added file

---
+++ src/main/java/com/app/demo/mavenjavaexample/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.mavenjavaexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "HARI NAGI REDDY KATTA - 22BD1A052V" );
+    }
+}
